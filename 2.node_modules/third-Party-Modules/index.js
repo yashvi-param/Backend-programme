2a89d72ede8bf1bab69c5077badb1e45cd7d64de
yashvi-param <yashvirwn123@gmail.com>
2026-01-28 19:05:15 +0530

    Add files via upload
    complete

---
+++ 2.node_modules/third-Party-Modules/index.js
@@ -0,0 +1,16 @@
+import http from "http";
+
+const server = http.createServer((req ,res)  =>{
+    res.end("Hello from node server");
+});
+
+const port=5000;
+
+server.listen(port, (err) => {
+    if(err){
+        console.log(err);
+        return;
+    }else{
+        console.log("server running on port",port);
+    }
+});
