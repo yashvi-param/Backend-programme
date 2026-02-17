f13a9c7afbb218ad0fcfb8a89f5b9c5af6306de4
yashvi-param <yashvirwn123@gmail.com>
2026-02-17 23:04:28 +0530

    Add files via upload
    complete

---
+++ 8.StudentManagementSystem-Mongo/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+
+import HttpError from "./middleware/HttpError.js";
+import connectDB from "./db/mongoose.js";
+import studentRouter from "./routers/studentRouter.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("students", studentRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).json("hello from server");
+});
+
+// undefined routes
+
+app.use((req, res, next) => {
+  next(new HttpError("requested route not found", 404));
+});
+
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    next(error);
+  }
+
+  res
+    .status(error.statusCode || 500)
+    .json({ message: error.message || "internal server error" });
+});
+
+const port = process.env.port || 5000;
+
+async function startServer() {
+  try {
+    await connectDB();
+
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+}
+
+startServer();
