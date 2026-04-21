70f1e5b205c02ca3547301ecc0ee535e68218e08
yashvi-param <yashvirwn123@gmail.com>
2026-04-21 22:49:27 +0530

    Add files via upload
    complete

---
+++ 13.quickNest/utils/sendEmail.js
@@ -0,0 +1,28 @@
+
+import transporter from "../config/email.js"
+
+
+
+const sendEmail = async ({ to, subject, html }) => {
+
+    try {
+
+        const info = await transporter.sendMail({
+            from:'Quick Nest "rnw.shivam.s@gmail.com"',
+            to,
+            subject,
+            html
+
+        })
+
+
+        console.log("email sent id", info.messageId);
+
+    } catch (error) {
+        console.log(error.message)
+
+
+    }
+}
+
+export default sendEmail
